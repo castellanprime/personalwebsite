0a2c3388e5b8c28a046922f16032245d77a64643
castellanprime <okusanya.damilola@gmail.com>
2017-07-11 15:12:23 -0500

    Added new routes for index, home, skills

--- mainweb/app.py
+++ mainweb/app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/home')
 def home():
 	return render_template('home.html')
+
+@app.route('/skills')
+def skills():
+	return render_template('skills.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__=="__main__":
 	app.run(host="0.0.0.0", port=5000)
